5664f4389a0c51d51671c77884f615d00a196faf
spoddub <spoddub98@gmail.com>
2023-01-29 10:53:35 +0300

    brain_games/scripts/brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import prompt
+from brain_games import cli
 
 def main():
-    print("Welcome to the Brain Games!")
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
