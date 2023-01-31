3698d0850ba23b13032974ce75673ff06b7ece1b
spoddub <spoddub98@gmail.com>
2023-01-31 08:28:15 +0300

    even_game.py

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+LOWER_LIMIT = 1
+UPPER_LIMIT = 200
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def get_round_data():
+	random_number = randint(LOWER_LIMIT, UPPER_LIMIT)
+
+	if is_even(random_number):
+		correct_answer = 'yes'
+	else:
+		correct_answer = 'no'
+	question = random_number
+	return question, correct_answer
+
+
+def is_even(random_number):
+	return random_number % 2 == 0
